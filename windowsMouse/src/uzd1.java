3185af1b18c6320567181275a0753cfad1393327
Vitalijs Muzalovs <vm@ssbsia.lv>
2022-09-15 20:40:54 +0300

    upd

---
+++ windowsMouse/src/uzd1.java
@@ -0,0 +1,37 @@
+import java.awt.event.*;
+import java.awt.*;
+
+public class Main implements MouseMotionListener, MouseListener{
+    private Frame f;
+    private TextField tf;
+
+    public Main(){
+        f = new Frame("Title");
+        tf = new TextField(30);
+    }
+
+public void launchFrame(){
+    Label lb = new Label("Klikšķini un kustini peli");
+    f.add(lb,BorderLayout.NORTH);
+    f.add(tf,BorderLayout.SOUTH);
+    f.addMouseMotionListener(this);
+    f.addMouseListener(this);
+    f.setSize(500,300);
+    f.setVisible(true);
+}
+
+public void mouseDragged(MouseEvent e){
+        String xy = "Peles vilkšana: X=" + e.getX() + " Y=" + e.getY();
+        tf.setText(xy);
+}
+
+    public void mouseMoved(MouseEvent e){}
+    public void mousePressed(MouseEvent e){}
+    public void mouseClicked(MouseEvent e){}
+    public void mouseReleased(MouseEvent e){}
+
+    public static void main(String[] args) {
+        Main two = new Main();
+        two.launchFrame();
+    }
+}
